2b0a1f315c1230141bc8aa17799793d930d428be
italo-wolf <italoparanista@hotmail.com>
2019-12-02 01:06:35 -0200

    Criando a estrutura de cadastro de Usuarios

---
+++ user/User.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/dataBaseCnn");
+
+const User = connection.define('users',{
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+User.sync({force: false}); 
+module.exports = User;
